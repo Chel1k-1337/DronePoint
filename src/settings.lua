4a5346ab8cbcac38867125f3b7ff9d5405866fe3
Chel1k <vladglusak2010@gmail.com>
2026-04-27 13:29:29 +0300

    Settings: Disable all modules by default

--- src/settings.lua
+++ src/settings.lua
@@ -1,16 +1,16 @@
 local Settings = {
     Drones = {
-        FPV = { Enabled = true, Color = Color3.fromRGB(255, 0, 0), Names = {"FPV", "Quadcopter", "Expert"} },
-        Bober = { Enabled = true, Color = Color3.fromRGB(255, 0, 0), Names = {"Bober", "Бобер", "bbrn"} },
-        Shahed136 = { Enabled = true, Color = Color3.fromRGB(255, 165, 0), Names = {"Shahed", "Geran", "Kamikaze", "Герань", "dronenight", "droneday"} },
-        Gerbera = { Enabled = true, Color = Color3.fromRGB(255, 165, 0), Names = {"Gerbera", "Гербера", "GrbrBl"} },
-        Lancet = { Enabled = true, Color = Color3.fromRGB(255, 0, 0), Names = {"Lancet"} }
+        FPV = { Enabled = false, Color = Color3.fromRGB(255, 0, 0), Names = {"FPV", "Quadcopter", "Expert"} },
+        Bober = { Enabled = false, Color = Color3.fromRGB(255, 0, 0), Names = {"Bober", "Бобер", "bbrn"} },
+        Shahed136 = { Enabled = false, Color = Color3.fromRGB(255, 165, 0), Names = {"Shahed", "Geran", "Kamikaze", "Герань", "dronenight", "droneday"} },
+        Gerbera = { Enabled = false, Color = Color3.fromRGB(255, 165, 0), Names = {"Gerbera", "Гербера", "GrbrBl"} },
+        Lancet = { Enabled = false, Color = Color3.fromRGB(255, 0, 0), Names = {"Lancet"} }
     },
     Rockets = {
-        Missile = { Enabled = true, Color = Color3.fromRGB(255, 255, 0), Names = {"Missile", "Rocket", "Projectile", "Neptune", "Нептун", "Ballistic", "H"} }
+        Missile = { Enabled = false, Color = Color3.fromRGB(255, 255, 0), Names = {"Missile", "Rocket", "Projectile", "Neptune", "Нептун", "Ballistic", "H"} }
     },
     Players = { Enabled = false, Color = Color3.fromRGB(0, 255, 0) },
-    Givers = { Enabled = true, Color = Color3.fromRGB(0, 255, 255), Names = {"Giver", "Stand", "Table", "Стол", "Выдача"} },
+    Givers = { Enabled = false, Color = Color3.fromRGB(0, 255, 255), Names = {"Giver", "Stand", "Table", "Стол", "Выдача"} },
     Universal = { Enabled = false, Color = Color3.fromRGB(255, 255, 255) },
     Visuals = { Style = "Highlight", FillOpacity = 0.5, OutlineOpacity = 0 }
 }
